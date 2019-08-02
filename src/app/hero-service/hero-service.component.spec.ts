927730c6c9cea15d40e62f205886005a556ee8a8
nosv <ing.geol@gmail.com>
2019-08-02 11:48:02 +0200

    Seventh an last chapter from Angular Tutorial Tour of Heroes.
    In this last chapter serach and delete buttons have been added.

---
+++ src/app/hero-service/hero-service.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeroServiceComponent } from './hero-service.component';
+
+describe('HeroServiceComponent', () => {
+  let component: HeroServiceComponent;
+  let fixture: ComponentFixture<HeroServiceComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HeroServiceComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeroServiceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
